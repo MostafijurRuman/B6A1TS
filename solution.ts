8029c6cd168c255a4c04c8b7ec1205a5b68aa030
MostafijurRuman <mustafizur.ruman7@gmail.com>
2025-11-16 23:13:07 +0600

    Soln : Problem 2,3

--- solution.ts
+++ solution.ts
@@ -7,7 +7,27 @@ const formatValue = (
   else if (typeof value === "number") {
     return value * 10;
   }
-  else {
+  else if(typeof value === "boolean") {
     return !value;
   }
+  throw new Error("Invalid Type")
 };
+
+const getLength = <T>(value: string | T[]): number => {
+  if (typeof value === "string") {
+    return value.length;
+  } 
+  else if (Array.isArray(value)) {
+    return value.length;
+  }
+
+  throw new Error("Invalid type");
+};
+
+class Person{
+   constructor( public name:string, public age:number){
+   }
+   getDetails():string{
+   return `Name: ${this.name}, Age: ${this.age}`;
+   }
+}
